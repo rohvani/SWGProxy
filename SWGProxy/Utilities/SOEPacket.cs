1c991013ab3d9cba960d57058136ab76155ebd4d
Seefo <rezen1911@gmail.com>
2014-11-21 02:44:08 -0500

    Began work on SOEPacket wrapper

---
+++ SWGProxy/Utilities/SOEPacket.cs
@@ -0,0 +1,41 @@
+﻿using SWGProxy.Packets;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SWGProxy.Utilities
+{
+	public class SOEPacket
+	{
+		private List<SWGPacket> childPackets = new List<SWGPacket>();
+		private bool xorModified = false;
+
+		public SOEPacket(byte[] data)
+		{
+			// ? decompress packet
+			// decrypt packet
+			// parse SWG packets and add to childPackets list
+		}
+
+		#region Packet Utility Methods
+		public byte[] ToArray()
+		{
+			List<byte> soeBuffer = new List<byte>();
+			// add SOE header
+			List<byte> swgPacketBuffer = new List<byte>();
+			foreach (SWGPacket packet in childPackets)
+			{
+				swgPacketBuffer.Add((byte)packet.ToArray().Length);
+				swgPacketBuffer.AddRange(packet.ToArray());
+			}
+			// encrypt swgpacket buffer
+			soeBuffer.AddRange(swgPacketBuffer);
+			// add SOE footer
+			// ? compress packet
+			return soeBuffer.ToArray();
+		}
+		#endregion
+	}
+}
